541d2100a7ccbd65132a0f78c16b4d0e0c303127
shlomi kassman <shlomikassman@shlomis-MacBook-Pro.local>
2024-11-11 17:06:58 +0200

    test aws 2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14
+FROM node:15
 
 WORKDIR /usr/src/app
 
@@ -6,7 +6,7 @@ COPY package.json .
 RUN npm install 
 COPY . .
 
-EXPOSE 8080
+EXPOSE 3000
 
 CMD ["node", "index.js"]
 
--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get('/hello',(req, res)=>{
 
 })
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on http://localhost:${port}`);
-})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
