8ce54d06f6152d6bb1958628e9c2b97da20478fb
Innovation_test_repo <you@example.com>
2024-11-13 08:30:52 +0200

    CI/CD

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3000;
 const config = require('config')
 
 app.get('/', (req, res)=>{
-    res.send('CI/CD App, Works well !')
+    res.send('CI/CD App, Works well !!!')
 })
 
 app.get('/status', (req, res)=>{
     res.status(200)
-    res.send('Okay')
+    res.send('Okay.')
 })
 
 app.get('/hello',(req, res)=>{
